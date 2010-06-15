8ccdc88551dc31faa99e872d4d6094621041ee37
Dan Allen <dan.j.allen@gmail.com>
2010-06-14 20:29:13 -0400

    license headers author tag add Asset impl to WebAppDef

--- src/main/java/org/jboss/shrinkwrap/descriptor/spec/web/WebAppDef.java
+++ src/main/java/org/jboss/shrinkwrap/descriptor/spec/web/WebAppDef.java
@@ -1,9 +1,36 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.shrinkwrap.descriptor.spec.web;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.math.BigInteger;
 import java.util.EventListener;
 
-public class WebAppDef
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
+import org.jboss.shrinkwrap.api.Asset;
+
+/**
+ * @author Dan Allen
+ */
+public class WebAppDef implements Asset
 {
    protected WebApp webApp;
    
@@ -184,6 +211,26 @@ public class WebAppDef
    public WebApp descriptor()
    {
       return webApp;
+   }
+   
+   @Override
+   public InputStream openStream()
+   {
+      try
+      {
+         JAXBContext context = JAXBContext.newInstance(WebApp.class);
+         Marshaller m = context.createMarshaller();
+         m.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, webApp.getSchemaLocation());
+         m.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+         m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+         ByteArrayOutputStream os = new ByteArrayOutputStream();
+         m.marshal(webApp, os);
+         return new ByteArrayInputStream(os.toByteArray());
+      }
+      catch (Exception e)
+      {
+         throw new RuntimeException("Could not convert descriptor to XML", e);
+      }
    }
    
    private String getSimpleName(String fqcn)
