5d20e40b5a64aefb59c62b8ac814a35a0da337f5
Aslak Knutsen <aslak@redhat.com>
2010-09-18 18:48:10 +0200

    SHRINKDESC-11 Add EE Application.xml support
    Supports displayName, description, modules(ejb, jar, connector, web) and libraryDirectory.

---
+++ api/src/main/java/org/jboss/shrinkwrap/descriptor/impl/spec/ee/application/ApplicationDescriptorImpl.java
@@ -0,0 +1,148 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.shrinkwrap.descriptor.impl.spec.ee.application;
+
+import org.jboss.shrinkwrap.descriptor.api.spec.ee.application.ApplicationDescriptor;
+import org.jboss.shrinkwrap.descriptor.impl.base.SchemaDescriptorImplBase;
+
+/**
+ * ApplicationDescriptorImpl
+ *
+ * @author <a href="mailto:aslak@redhat.com">Aslak Knutsen</a>
+ * @version $Revision: $
+ */
+public class ApplicationDescriptorImpl extends SchemaDescriptorImplBase<ApplicationModel> implements ApplicationDescriptor
+{
+   //-------------------------------------------------------------------------------------||
+   // Instance Members -------------------------------------------------------------------||
+   //-------------------------------------------------------------------------------------||
+
+   private ApplicationModel model;
+   
+   //-------------------------------------------------------------------------------------||
+   // Constructor ------------------------------------------------------------------------||
+   //-------------------------------------------------------------------------------------||
+
+   public ApplicationDescriptorImpl()
+   {
+      this(new ApplicationModel());
+      
+   }
+   
+   public ApplicationDescriptorImpl(ApplicationModel model)
+   {
+      this.model = model;
+   }
+   
+   //-------------------------------------------------------------------------------------||
+   // API --------------------------------------------------------------------------------||
+   //-------------------------------------------------------------------------------------||
+
+   /* (non-Javadoc)
+    * @see org.jboss.shrinkwrap.descriptor.api.spec.ee.application.ApplicationDescriptor#description()
+    */
+   @Override
+   public ApplicationDescriptor description(String description)
+   {
+      model.setDescription(description);
+      return this;
+   }
+   
+   /* (non-Javadoc)
+    * @see org.jboss.shrinkwrap.descriptor.api.spec.ee.application.ApplicationDescriptor#displayName(java.lang.String)
+    */
+   @Override
+   public ApplicationDescriptor displayName(String displayName)
+   {
+      model.setDisplayName(displayName);
+      return this;
+   }
+   
+   /* (non-Javadoc)
+    * @see org.jboss.shrinkwrap.descriptor.api.spec.ee.application.ApplicationDescriptor#libraryDirectory(java.lang.String)
+    */
+   @Override
+   public ApplicationDescriptor libraryDirectory(String libraryDirectory)
+   {
+      model.setLibraryDirectory(libraryDirectory);
+      return this;
+   }
+   
+   /* (non-Javadoc)
+    * @see org.jboss.shrinkwrap.descriptor.api.spec.ee.application.ApplicationDescriptor#ejbModule(java.lang.String)
+    */
+   public ApplicationDescriptor ejbModule(String uri) 
+   {
+      model.getEjbModules().add(uri);
+      return this;
+   }
+   
+   /* (non-Javadoc)
+    * @see org.jboss.shrinkwrap.descriptor.api.spec.ee.application.ApplicationDescriptor#javaModule(java.lang.String)
+    */
+   @Override
+   public ApplicationDescriptor javaModule(String uri)
+   {
+      model.getJavaModules().add(uri);
+      return this;
+   }
+   
+   /* (non-Javadoc)
+    * @see org.jboss.shrinkwrap.descriptor.api.spec.ee.application.ApplicationDescriptor#connectorModule(java.lang.String)
+    */
+   @Override
+   public ApplicationDescriptor connectorModule(String uri)
+   {
+      model.getConnectorModules().add(uri);
+      return this;
+   }
+   
+   /* (non-Javadoc)
+    * @see org.jboss.shrinkwrap.descriptor.api.spec.ee.application.ApplicationDescriptor#webModule(java.lang.String, java.lang.String)
+    */
+   @Override
+   public ApplicationDescriptor webModule(String uri, String contextRoot)
+   {
+      model.getWebModules().add(new WebModuleImpl(uri, contextRoot));
+      return this;
+   }
+   
+   /* (non-Javadoc)
+    * @see org.jboss.shrinkwrap.descriptor.api.spec.ee.application.ApplicationDescriptor#version(java.lang.String)
+    */
+   @Override
+   public ApplicationDescriptor version(String version)
+   {
+      model.setVersion(version);
+      return this;
+   }
+   
+   
+   //-------------------------------------------------------------------------------------||
+   // Required Implementations - SchemaDescriptorImplBase --------------------------------||
+   //-------------------------------------------------------------------------------------||
+
+   /* (non-Javadoc)
+    * @see org.jboss.shrinkwrap.descriptor.spi.SchemaDescriptorProvider#getSchemaModel()
+    */
+   @Override
+   public ApplicationModel getSchemaModel()
+   {
+      return model;
+   }
+
+}
---
+++ api/src/main/java/org/jboss/shrinkwrap/descriptor/impl/spec/ee/application/ApplicationModel.java
@@ -0,0 +1,216 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.shrinkwrap.descriptor.impl.spec.ee.application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchema;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+
+import org.jboss.shrinkwrap.descriptor.spi.SchemaModel;
+
+/**
+ * ApplicationModel
+ *
+ * @author <a href="mailto:aslak@redhat.com">Aslak Knutsen</a>
+ * @version $Revision: $
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+})
+@XmlRootElement(name = "application")
+public class ApplicationModel implements SchemaModel
+{
+   public static final String DEFAULT_VERSION = "6";
+   
+   //-------------------------------------------------------------------------------------||
+   // Instance Members -------------------------------------------------------------------||
+   //-------------------------------------------------------------------------------------||
+   
+   @XmlAttribute(name = "version")
+   private String version = DEFAULT_VERSION;
+
+   @XmlElement(name = "description")
+   private String description;
+   
+   @XmlElement(name = "display-name")
+   private String displayName;
+   
+   @XmlElement(name = "library-directory")
+   private String libraryDirectory;
+   
+   @XmlElementWrapper(name = "module")
+   @XmlElement(name = "ejb")
+   private List<String> ejbModules;
+   
+   @XmlElementWrapper(name = "module")
+   @XmlElement(name = "web")
+   private List<WebModuleImpl> webModules;
+   
+   @XmlElementWrapper(name = "module")
+   @XmlElement(name = "connector")
+   private List<String> connectorModules;
+   
+   @XmlElementWrapper(name = "module")
+   @XmlElement(name = "java")
+   private List<String> javaModules;
+   
+   /**
+    * @param description the description to set
+    */
+   public void setDescription(String description)
+   {
+      this.description = description;
+   }
+   
+   /**
+    * @return the description
+    */
+   public String getDescription()
+   {
+      return description;
+   }
+   
+   /**
+    * @param displayName the displayName to set
+    */
+   public void setDisplayName(String displayName)
+   {
+      this.displayName = displayName;
+   }
+
+   /**
+    * @return the displayName
+    */
+   public String getDisplayName()
+   {
+      return displayName;
+   }
+   
+   /**
+    * @param version the version to set
+    */
+   public void setVersion(String version)
+   {
+      this.version = version;
+   }
+   
+   /**
+    * @param libraryDirectory the libraryDirectory to set
+    */
+   public void setLibraryDirectory(String libraryDirectory)
+   {
+      this.libraryDirectory = libraryDirectory;
+   }
+   
+   /**
+    * @return the libraryDirectory
+    */
+   public String getLibraryDirectory()
+   {
+      return libraryDirectory;
+   }
+   
+   /**
+    * @return the ejbModules
+    */
+   public List<String> getEjbModules()
+   {
+      if(ejbModules == null)
+      {
+         ejbModules = new ArrayList<String>();
+      }
+      return ejbModules;
+   }
+   
+   /**
+    * @return the webModules
+    */
+   public List<WebModuleImpl> getWebModules()
+   {
+      if(webModules == null)
+      {
+         webModules = new ArrayList<WebModuleImpl>();
+      }
+      return webModules;
+   }
+   
+   /**
+    * @return the javaModules
+    */
+   public List<String> getJavaModules()
+   {
+      if(javaModules == null)
+      {
+         javaModules = new ArrayList<String>();
+      }
+      return javaModules;
+   }
+   
+   /**
+    * @return the connectorModules
+    */
+   public List<String> getConnectorModules()
+   {
+      if(connectorModules == null)
+      {
+         connectorModules = new ArrayList<String>();
+      }
+      return connectorModules;
+   }
+   
+   //-------------------------------------------------------------------------------------||
+   // Required Implementations - SchemaModel ---------------------------------------------||
+   //-------------------------------------------------------------------------------------||
+
+   /* (non-Javadoc)
+    * @see org.jboss.shrinkwrap.descriptor.spi.SchemaModel#getVersion()
+    */
+   @Override
+   public String getVersion()
+   {
+      return version;
+   }
+
+   /* (non-Javadoc)
+    * @see org.jboss.shrinkwrap.descriptor.spi.SchemaModel#getNamespace()
+    */
+   @Override
+   public String getNamespace()
+   {
+      return ApplicationModel.class.getPackage().getAnnotation(XmlSchema.class).namespace();
+   }
+
+   /* (non-Javadoc)
+    * @see org.jboss.shrinkwrap.descriptor.spi.SchemaModel#getSchemaLocation()
+    */
+   @Override
+   public String getSchemaLocation()
+   {
+      final String namespace = getNamespace();
+      return new StringBuilder().append(namespace).append(" ").append(namespace).append("/application_")
+            .append(getVersion().replace('.', '_')).append(".xsd").toString();
+   }
+}
