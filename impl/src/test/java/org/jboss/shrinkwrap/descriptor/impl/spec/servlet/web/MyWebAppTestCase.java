cf701bbfd24de6c9b15e0d2a9c931dd11ebfaaba
battenfeld <bfr@bfr-laptop.(none)>
2011-05-12 23:38:02 +0200

    Committer: battenfeld <bfr@bfr-laptop.(none)>
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
    
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	deleted:    api/src/main/java/org/jboss/shrinkwrap/descriptor/api/webcommon30/WebAppDescriptor.java.bak
    	modified:   impl/src/test/java/org/jboss/shrinkwrap/descriptor/impl/spec/servlet/web/MyWebAppTestCase.java

--- impl/src/test/java/org/jboss/shrinkwrap/descriptor/impl/spec/servlet/web/MyWebAppTestCase.java
+++ impl/src/test/java/org/jboss/shrinkwrap/descriptor/impl/spec/servlet/web/MyWebAppTestCase.java
@@ -1,7 +1,7 @@
 package org.jboss.shrinkwrap.descriptor.impl.spec.servlet.web;
 
 import org.jboss.shrinkwrap.descriptor.api.Descriptors;
-import org.jboss.shrinkwrap.descriptor.api.javaee6.webapp.WebAppDescriptor;
+import org.jboss.shrinkwrap.descriptor.api.webapp30.WebAppDescriptor;
 import org.junit.Test;
 
 public class MyWebAppTestCase
@@ -11,9 +11,8 @@ public class MyWebAppTestCase
    public void myFirstTest() throws Exception
    {
       final String webApp = Descriptors.create(WebAppDescriptor.class)
-         .version("3.0")
-         .absoluteOrdering().name("", "", "").others().up()
-         .securityConstraint().displayName("").authConstraint().description("").roleName("").up().up()
+         .absoluteOrdering().name("name1").name("name2").up()
+         .securityConstraint().displayName("displayName").authConstraint().description("description").roleName("manager").up().up()
          .filter().filterName("").filterClass("").asyncSupported(false).up()
          .exportAsString();
    }
