3274d32c362c36b5fc9c0273dc70f7bfb5e98422
bfr <bfr@bfr-N73JQ.(none)>
2011-05-14 14:06:45 +0200

    Committer: bfr <bfr@bfr-N73JQ.(none)>
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
    
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   api/src/main/java/org/jboss/shrinkwrap/descriptor/api/webcommon30/ServletMappingType.java
    	new file:   api/src/main/resources/dd.xsl
    
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	api/src/main/java/org/jboss/shrinkwrap/descriptor/api/webcommon30/webcommon30.txt

--- api/src/main/java/org/jboss/shrinkwrap/descriptor/api/webcommon30/ServletMappingType.java
+++ api/src/main/java/org/jboss/shrinkwrap/descriptor/api/webcommon30/ServletMappingType.java
@@ -1,6 +1,16 @@
 package org.jboss.shrinkwrap.descriptor.api.webcommon30;
 
-public interface ServletMappingType
-{
+import org.jboss.shrinkwrap.descriptor.api.Child;
 
+/**
+ * ServletMappingType
+ * 
+ * @author <a href="mailto:ralf.battenfeld@bluewin.ch">Ralf Battenfeld</a>
+ * @version $Revision: $
+ */
+public interface ServletMappingType<T> extends Child<T>
+{
+   public ServletMappingType<T> servletName(String servletName);
+
+   public ServletMappingType<T> urlPattern(String urlPattern);
 }
