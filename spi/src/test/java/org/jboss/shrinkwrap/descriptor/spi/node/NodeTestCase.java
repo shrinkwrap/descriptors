e6d8b0862e754b0c79b6fee8424b663aa6273467
Bartosz Majsak <bartosz.majsak@gmail.com>
2011-09-06 23:55:26 +0200

    [SHRINKDESC-84] Fixed tests to use only Node operations.

--- spi/src/test/java/org/jboss/shrinkwrap/descriptor/spi/node/NodeTestCase.java
+++ spi/src/test/java/org/jboss/shrinkwrap/descriptor/spi/node/NodeTestCase.java
@@ -21,10 +21,7 @@ import static org.jboss.shrinkwrap.descriptor.spi.node.query.TestTreeBuilder.*;
 import java.util.List;
 import java.util.Map;
 
-import org.jboss.shrinkwrap.descriptor.spi.node.Node;
 import org.jboss.shrinkwrap.descriptor.spi.node.query.Pattern;
-import org.jboss.shrinkwrap.descriptor.spi.node.query.Patterns;
-import org.jboss.shrinkwrap.descriptor.spi.node.query.queries.AbsoluteGetSingleQuery;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -49,13 +46,13 @@ public class NodeTestCase
       Node parent = new Node(ROOT_NODE);
       Node child = new Node("a name", parent);
    }
-   
+
    @Test(expected = IllegalArgumentException.class)
    public void shouldNotAllowCreationOfTreeWithEmptyStringAsRootName()
    {
       Node root = new Node("");
    }
-   
+
    @Test(expected = IllegalArgumentException.class)
    public void shouldNotAllowCreationOfChildWithBlankStringAsNodeName()
    {
@@ -68,9 +65,7 @@ public class NodeTestCase
       Node root = new Node(ROOT_NODE);
       Node child = root.createChild(CHILD_1_NODE);
 
-      Assert.assertEquals(
-               "Verify ability to get parent node",
-               root, child.getParent());
+      Assert.assertEquals("Verify ability to get parent node", root, child.getParent());
    }
 
    @Test
@@ -80,13 +75,9 @@ public class NodeTestCase
       Node child1 = root.getOrCreate(CHILD_1_NODE);
       Node child1_ref = root.getOrCreate(CHILD_1_NODE);
 
-      Assert.assertEquals(
-               "Verify root only has one child",
-               1, root.getChildren().size());
+      Assert.assertEquals("Verify root only has one child", 1, root.getChildren().size());
 
-      Assert.assertEquals(
-            "Verify the previous created node was returned",
-            child1, child1_ref);
+      Assert.assertEquals("Verify the previous created node was returned", child1, child1_ref);
    }
 
    @Test
@@ -96,13 +87,9 @@ public class NodeTestCase
       Node child1 = root.createChild(CHILD_1_NODE);
       Node child2 = root.createChild(CHILD_1_NODE);
 
-      Assert.assertEquals(
-               "Verify root only has two children",
-               2, root.getChildren().size());
+      Assert.assertEquals("Verify root only has two children", 2, root.getChildren().size());
 
-      Assert.assertNotSame(
-            "Verify the children are not the same object",
-            child1, child2);
+      Assert.assertNotSame("Verify the children are not the same object", child1, child2);
    }
 
    @Test
@@ -115,17 +102,11 @@ public class NodeTestCase
 
       List<Node> found = root.get(CHILD_1_NODE);
 
-      Assert.assertEquals(
-               "Verify only the named nodes were found",
-               2, found.size());
+      Assert.assertEquals("Verify only the named nodes were found", 2, found.size());
 
-      Assert.assertEquals(
-               "Verify the correct node was found",
-               child1, found.get(0));
+      Assert.assertEquals("Verify the correct node was found", child1, found.get(0));
 
-      Assert.assertEquals(
-               "Verify the correct node was found",
-               child2, found.get(1));
+      Assert.assertEquals("Verify the correct node was found", child2, found.get(1));
    }
 
    @Test
@@ -136,9 +117,7 @@ public class NodeTestCase
 
       Node found = root.getSingle(CHILD_1_NODE);
 
-      Assert.assertEquals(
-            "Verify correct node was found",
-            child, found);
+      Assert.assertEquals("Verify correct node was found", child, found);
    }
 
    @Test(expected = IllegalArgumentException.class)
@@ -155,45 +134,33 @@ public class NodeTestCase
    @Test
    public void shouldBeAbleToReadAndWriteAttribute() throws Exception
    {
-      Node root = new Node(ROOT_NODE)
-               .attribute(ATTR_NAME, ATTR_VALUE_1);
+      Node root = new Node(ROOT_NODE).attribute(ATTR_NAME, ATTR_VALUE_1);
 
-      Assert.assertEquals(
-               "Verify abillity to store attribues",
-               root.getAttribute(ATTR_NAME), ATTR_VALUE_1);
+      Assert.assertEquals("Verify abillity to store attribues", root.getAttribute(ATTR_NAME), ATTR_VALUE_1);
    }
 
    @Test
    public void shouldBeAbleToReadAndWriteAttributeObject() throws Exception
    {
-      Node root = new Node(ROOT_NODE)
-               .attribute(ATTR_NAME, new StringBuilder(ATTR_VALUE_1));
+      Node root = new Node(ROOT_NODE).attribute(ATTR_NAME, new StringBuilder(ATTR_VALUE_1));
 
-      Assert.assertEquals(
-               "Verify abillity to store attribues",
-               root.getAttribute(ATTR_NAME), ATTR_VALUE_1);
+      Assert.assertEquals("Verify abillity to store attribues", root.getAttribute(ATTR_NAME), ATTR_VALUE_1);
    }
 
    @Test
    public void shouldBeAbleToReadWriteTextBody() throws Exception
    {
-      Node root = new Node(ROOT_NODE)
-               .text(BODY);
+      Node root = new Node(ROOT_NODE).text(BODY);
 
-      Assert.assertEquals(
-               "Verify abillity to store text body",
-               BODY, root.getText());
+      Assert.assertEquals("Verify abillity to store text body", BODY, root.getText());
    }
 
    @Test
    public void shouldBeAbleToReadWriteTextBodyObject() throws Exception
    {
-      Node root = new Node(ROOT_NODE)
-               .text(new StringBuilder(BODY));
+      Node root = new Node(ROOT_NODE).text(new StringBuilder(BODY));
 
-      Assert.assertEquals(
-               "Verify abillity to store text body",
-               BODY, root.getText());
+      Assert.assertEquals("Verify abillity to store text body", BODY, root.getText());
    }
 
    @Test
@@ -224,7 +191,7 @@ public class NodeTestCase
 
       root.createChild(childName);
       Assert.assertNull(root.getTextValueForPatternName(childName));
-      
+
       root.getChildren().get(0).text(childText);
       Assert.assertNotNull(root.getTextValueForPatternName(childName));
       Assert.assertEquals(childText, root.getTextValueForPatternName(childName));
@@ -275,7 +242,8 @@ public class NodeTestCase
    }
 
    @Test
-   public void assertToStringFormat() throws Exception {
+   public void assertToStringFormat() throws Exception
+   {
       Node root = new Node(ROOT_NODE);
       String r = root.toString();
       Assert.assertTrue(r.startsWith(root.getClass().getSimpleName()));
@@ -417,27 +385,30 @@ public class NodeTestCase
       List<Node> removedNodes = root.removeChildren(pattern);
       Assert.assertTrue(removedNodes.isEmpty());
    }
-   
+
    @Test
-   public void shouldNotBeCommentByDefault(){
+   public void shouldNotBeCommentByDefault()
+   {
       final Node node = new Node(ROOT_NODE);
       Assert.assertEquals("A Node should not be a comment by default", false, node.isComment());
    }
-   
+
    @Test
-   public void shouldBeAbleToMarkAsComment(){
+   public void shouldBeAbleToMarkAsComment()
+   {
       final Node node = new Node(ROOT_NODE);
       node.setComment(true);
       Assert.assertEquals("A Node set as comment should report as comment", true, node.isComment());
    }
-   
-   @Test(expected=IllegalArgumentException.class)
-   public void shouldNotBeAbleToSetNodeWitHChildrenAsComment(){
+
+   @Test(expected = IllegalArgumentException.class)
+   public void shouldNotBeAbleToSetNodeWitHChildrenAsComment()
+   {
       final Node node = new Node(ROOT_NODE);
       node.createChild(CHILD_1_NODE);
       node.setComment(true);
    }
-   
+
    @Test
    public void shouldBeAbleToAccessRootFromChildNode()
    {
@@ -447,30 +418,26 @@ public class NodeTestCase
 
       // when
       final Node actualRoot = child.getRoot();
-      
+
       // then
       Assert.assertEquals("Obtained wrong root", root, actualRoot);
    }
-   
+
    @Test
    public void shouldBeAbleToAccessRootAnyDescendantNode()
    {
       // given
       final Node root = new Node(ROOT_NODE);
-      final Node descendantNode = root.createChild(CHILD_1_NODE)
-                                         .createChild(CHILD_1_1_NODE).getParent().getParent()
-                                      .createChild(CHILD_2_NODE)
-                                         .createChild(CHILD_2_1_NODE).getParent()
-                                         .createChild(CHILD_2_2_NODE);
+      final Node descendantNode = root.createChild(CHILD_1_NODE).createChild(CHILD_1_1_NODE).getParent().getParent()
+            .createChild(CHILD_2_NODE).createChild(CHILD_2_1_NODE).getParent().createChild(CHILD_2_2_NODE);
 
       // when
       final Node actualRoot = descendantNode.getRoot();
-      
+
       // then
       Assert.assertEquals("Obtained wrong root", root, actualRoot);
    }
 
-   
    /**
     * Ensures that {@link Node#isRoot()} is working as contracted
     */
@@ -489,7 +456,8 @@ public class NodeTestCase
       Node root = new Node(ROOT_NODE).attribute(ATTR_NAME, ATTR_VALUE_1);
       Assert.assertEquals("attribute should exist", root.getAttribute(ATTR_NAME), ATTR_VALUE_1);
 
-      Assert.assertEquals("attribute value should be returned on removal", root.removeAttribute(ATTR_NAME), ATTR_VALUE_1);
+      Assert.assertEquals("attribute value should be returned on removal", root.removeAttribute(ATTR_NAME),
+            ATTR_VALUE_1);
       Assert.assertNull("attribute should no longer exist", root.getAttribute(ATTR_NAME));
    }
 
@@ -503,47 +471,41 @@ public class NodeTestCase
       final String shouldBeNull = root.removeAttribute(bogusAttribute);
       Assert.assertNull("Attempting to remove an attribute which does not exist should return null", shouldBeNull);
    }
-   
+
    @Test(expected = IllegalArgumentException.class)
    public void removeNullAttributeShouldThrowException()
    {
       Node root = new Node(ROOT_NODE);
       root.removeAttribute(null);
    }
-   
+
    @Test
-   public void shouldNotMatchAChildsChildrenOnGet() 
+   public void shouldNotMatchAChildsChildrenOnGet()
    {
       // given /root/child1/child2
-      Node root = new Node(ROOT_NODE)
-                        .createChild(CHILD_1_NODE)
-                        .createChild(CHILD_2_NODE)
-                        .getRoot();
+      Node root = new Node(ROOT_NODE).createChild(CHILD_1_NODE).createChild(CHILD_2_NODE).getRoot();
 
       // when 
       List<Node> child = root.get(CHILD_2_NODE);
-      
+
       // then
       Assert.assertTrue("Should not find matching child", child.isEmpty());
    }
-   
+
    @Test
-   public void shouldNotMatchAChildsChildrenOnGetSingle() 
+   public void shouldNotMatchAChildsChildrenOnGetSingle()
    {
       // given /root/child1/child2
-      Node root = new Node(ROOT_NODE)
-                        .createChild(CHILD_1_NODE)
-                           .createChild(CHILD_2_NODE)
-                        .getRoot();
+      Node root = new Node(ROOT_NODE).createChild(CHILD_1_NODE).createChild(CHILD_2_NODE).getRoot();
       // when
       Node child = root.getSingle(CHILD_2_NODE);
-      
+
       // then
       Assert.assertNull(child);
    }
 
    @Test
-   public void shouldNotMatchAChildsChildrenOnGetOrCreate() 
+   public void shouldNotMatchAChildsChildrenOnGetOrCreate()
    {
       // given /root/child1/child2
       Node root = new Node(ROOT_NODE);
@@ -558,128 +520,96 @@ public class NodeTestCase
    }
 
    @Test
-   public void shouldNotMatchAChildsChildrenOnRemoveChild() 
+   public void shouldNotMatchAChildsChildrenOnRemoveChild()
    {
       // given /root/child1/child2
-      Node root = new Node(ROOT_NODE)
-                        .createChild(CHILD_1_NODE)
-                           .createChild(CHILD_2_NODE).getRoot();
+      Node root = new Node(ROOT_NODE).createChild(CHILD_1_NODE).createChild(CHILD_2_NODE).getRoot();
       // when
       Node removedChild = root.removeChild(CHILD_2_NODE);
-      
+
       // then
       Assert.assertNull(removedChild);
    }
 
    @Test
-   public void shouldNotMatchAChildsChildrenOnRemoveChildren() 
+   public void shouldNotMatchAChildsChildrenOnRemoveChildren()
    {
       // given /root/child1/child2
-      Node root = new Node(ROOT_NODE)
-                        .createChild(CHILD_1_NODE)
-                           .createChild(CHILD_2_NODE).getRoot();
+      Node root = new Node(ROOT_NODE).createChild(CHILD_1_NODE).createChild(CHILD_2_NODE).getRoot();
 
       // when
       List<Node> removed = root.removeChildren(CHILD_2_NODE);
-      
+
       // then
       Assert.assertNotNull(removed);
-      Assert.assertEquals(0,  removed.size());
+      Assert.assertEquals(0, removed.size());
    }
-   
+
    @Test
-   public void shouldBeAbleToFindAChildWithTextValueUsingAbsoluteQuery() throws Exception
+   public void shouldBeAbleToFindAChildWithTextValue() throws Exception
+   {
+      // given
+      Node root = createTree();
+
+      // when
+      Node found = root.getSingle(("/" + CHILD_3_NODE + "=" + CHILD_3_TEXT));
+
+      // then
+      Assert.assertEquals("Verify correct node found", CHILD_3_NODE, found.getName());
+      Assert.assertEquals("Verify correct node value", CHILD_3_TEXT, found.getText());
+   }
+
+   @Test
+   public void shouldBeAbleToFindAExpressedChild() throws Exception
    {
       // given
       Node root = createTree();
       
       // when
-      Node found = AbsoluteGetSingleQuery.INSTANCE.execute(root, Patterns.from("/" + ROOT_NODE + "/" + CHILD_3_NODE + "=" + CHILD_3_TEXT));
+      Node found = root.getSingle((CHILD_1_NODE + "/" + CHILD_1_1_NODE));
 
-      
-      // then
-      Assert.assertEquals(
-               "Verify correct node found",
-               CHILD_3_NODE, found.getName());
-      Assert.assertEquals(
-               "Verify correct node value",
-               CHILD_3_TEXT, found.getText());
-   }
-   
-   @Test
-   public void shouldBeAbleToFindAExpressedChild() throws Exception
-   {
-      Node root = createTree();
-      System.out.println(root.toString(true));
-      Node found = AbsoluteGetSingleQuery.INSTANCE.execute(root, Patterns.from("/" + ROOT_NODE + "/" + CHILD_1_NODE + "/" + CHILD_1_1_NODE));
-      
+      // when
       Assert.assertNotNull("Verify a node as found", found);
-      
-      Assert.assertEquals(
-            "Verify correct node found",
-            CHILD_1_1_NODE, found.getName());      
-   }
-   
-   @Test
-   public void shouldBeAbleToFindAExpressedFromRoot() throws Exception
-   {
-      Node root = createTree();
-      System.out.println(root.toString(true));
-      Node found = AbsoluteGetSingleQuery.INSTANCE.execute(root, Patterns.from("/" + ROOT_NODE + "/" + CHILD_1_NODE + "/" + CHILD_1_1_NODE));
-      
-      Assert.assertNotNull("Verify a node was found", found);
-      
-      Assert.assertEquals(
-            "Verify correct node found",
-            CHILD_1_1_NODE, found.getName());      
+
+      Assert.assertEquals("Verify correct node found", CHILD_1_1_NODE, found.getName());
    }
 
    @Test
-   public void shouldBeAbleToFindAExpressedFromRootWithExpression() throws Exception
+   public void shouldBeAbleToFindDescendantWithAttribute() throws Exception
    {
+      // given
       Node root = createTree();
-      Node found = AbsoluteGetSingleQuery.INSTANCE.execute(root, Patterns.from("/" + ROOT_NODE + "/" + CHILD_2_NODE + "/" + CHILD_2_1_NODE + "@" + ATTR_NAME + "=" + ATTR_VALUE_1));
       
-      System.out.println(root.toString(true));
-      Assert.assertNotNull("Verify a node was found", found);
-      
-      Assert.assertEquals(
-            "Verify correct node found",
-            CHILD_2_1_NODE, found.getName());      
+      // when
+      Node found = root.getSingle(("/" + CHILD_2_NODE + "/" 
+            + CHILD_2_1_NODE + "@" + ATTR_NAME + "=" + ATTR_VALUE_1));
 
-      Assert.assertEquals(
-            "Verify correct node found",
-            ATTR_VALUE_1, found.getAttribute(ATTR_NAME));      
+      // then
+      Assert.assertNotNull("Verify a node was found", found);
+
+      Assert.assertEquals("Verify correct node found", CHILD_2_1_NODE, found.getName());
+
+      Assert.assertEquals("Verify correct node found", ATTR_VALUE_1, found.getAttribute(ATTR_NAME));
    }
 
-
-   
    @Test
    public void shouldBeAbleToGetNodeWithTextValues()
    {
+      // given
       Node root = new Node(ROOT_NODE);
       root.getOrCreate(("/" + CHILD_2_NODE));
       root.getOrCreate(("/" + CHILD_3_NODE));
       root.getOrCreate(("/" + CHILD_3_NODE + "=" + CHILD_3_TEXT));
       root.getOrCreate(("/" + CHILD_3_NODE + "=" + CHILD_3_TEXT + "diff"));
 
-      Node found = AbsoluteGetSingleQuery.INSTANCE.execute(root, Patterns.from("/" + ROOT_NODE + "/" + CHILD_3_NODE + "=" + CHILD_3_TEXT));
+      // when
+      Node found = root.getSingle(("/" + CHILD_3_NODE + "=" + CHILD_3_TEXT));
 
+      // then
       Assert.assertNotNull("Verify node was found", found);
-      
-      Assert.assertEquals(
-               "Verify correct node created",
-               CHILD_3_NODE, found.getName());
-
-      Assert.assertEquals(
-               "Verify correct value set",
-               CHILD_3_TEXT, found.getText());
-
-      Assert.assertEquals(
-               "Verify root only has four children",
-               4, root.getChildren().size());
+      Assert.assertEquals("Verify correct node created", CHILD_3_NODE, found.getName());
+      Assert.assertEquals("Verify correct value set", CHILD_3_TEXT, found.getText());
+      Assert.assertEquals("Verify root only has four children", 4, root.getChildren().size());
    }
 
-   
 }
-   
